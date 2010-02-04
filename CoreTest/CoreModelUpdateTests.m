5f367a8b819f75b9fc21a56e555f03bbeb8167e9
Mike Laurence <mikelaurence@LaurenceLive.local>
2010-02-04 10:50:58 -0600

    added (failing) updateWithDictionary test

--- CoreTest/CoreModelUpdateTests.m
+++ CoreTest/CoreModelUpdateTests.m
@@ -13,7 +13,18 @@
 
 @implementation CoreModelUpdateTests
 
-- (void) testUpdateWithDictionary { GHFail(nil); }
+- (void) testUpdateWithDictionary { 
+    [self loadArtist:0];
+    NSDictionary* updateDict = [[self artistDataJSON:@"artists.0.update"] JSONValue];
+    
+    Artist* artist = (Artist*)[[Artist findLocal:[NSNumber numberWithInt:0]] resource];
+    [artist updateWithDictionary:updateDict];
+    
+    GHAssertEqualStrings(artist.name, @"Peter B. Gabriel", nil);
+    GHAssertEqualStrings(artist.summary, @"Peter Brian Gabriel is a musician and songwriter.", nil);
+    GHAssertEqualStrings(artist.description, @"Peter Brian Gabriel (born 13 February 1950) is an English musician and songwriter. He's quite good.", nil);
+}
+
 - (void) testShouldUpdateWithDictionary { GHFail(nil); }
 
 @end
