d27f0f04676e417155641a5ce59ba97cf74be8a2
Mike Laurence <mikelaurence@LaurenceLive.local>
2010-03-11 16:27:09 -0600

    added core data benchmarks

---
+++ Examples/CoreTest/CoreDataBenchmarks.m
@@ -0,0 +1,66 @@
+//
+//  CoreDataBenchmarks.m
+//  CoreTest
+//
+//  Created by Mike Laurence on 3/11/10.
+//  Copyright 2010 Mike Laurence. All rights reserved.
+//
+
+
+#import "CoreResourceTestCase.h"
+
+@interface CoreDataBenchmarks : CoreResourceTestCase {
+    // Cached pointers
+    Artist* artist;
+    NSString* value;
+    NSString* readKey;
+    NSString* writeKey;
+    SEL readSel, writeSel;
+}
+@end
+
+@implementation CoreDataBenchmarks
+
+#define ITERATE for (int i = 0; i < 1000000; i++)
+
+- (void) setUp {
+    artist = [[self loadArtist:0] retain];
+    readKey = [@"name" retain];
+    readSel = @selector(name);
+    writeKey = [@"setName" retain];
+    writeSel = @selector(setName:);
+}
+
+- (void) testReadsUsingDynamicGetters {
+    ITERATE value = artist.name;
+}
+
+- (void) testReadsUsingKVC {
+    ITERATE value = [artist valueForKey:readKey];
+}
+
+- (void) testReadsUsingPrimitiveKVC {
+    ITERATE value = [artist primitiveValueForKey:readKey];
+}
+
+- (void) testReadsUsingSelector {
+    ITERATE value = [artist performSelector:readSel];
+}
+
+- (void) testReadsUsingConvertedSelector {
+    ITERATE value = [artist performSelector:NSSelectorFromString(readKey)];
+}
+
+- (void) testWritesUsingDynamicSetters {
+    ITERATE artist.name = @"Schpoon";
+}
+
+- (void) testWritesUsingKVC {
+    ITERATE [artist setValue:@"Schpoon" forKey:@"name"];
+}
+
+- (void) testWritesUsingPerformSelector {
+    ITERATE value = [artist performSelector:writeSel withObject:@"Schpoon"];
+}
+
+@end
